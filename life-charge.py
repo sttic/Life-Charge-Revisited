e77ea134285508394da96f41d6e4fdb7e813d6cd
sttic <tommyd3689@gmail.com>
2018-03-18 22:30:29 -0400

    video generation

--- life-charge.py
+++ life-charge.py
@@ -1,5 +1,8 @@
 from PIL import Image, ImageDraw
 import datetime
+import numpy, cv2
+import subprocess, os
+from shutil import copyfile
 
 DOT_WIDTH = 1
 DOT_HEIGHT = 1
@@ -13,7 +16,8 @@ BORDER_VSPACE = 2
 PAD_WIDTH = 8
 PAD_HEIGHT = 8
 
-SCALE = 16
+SCALE_IMG = 16
+SCALE_VID = 4
 
 # in number of dots
 GRID_WIDTH = 52
@@ -32,7 +36,8 @@ GREY = 3*(8,)
 
 
 def get_DOB():
-    year, month, day = int(input("YYYY: ")), int(input("MM: ")), int(input("DD: "))
+    # year, month, day = int(input("YYYY: ")), int(input("MM: ")), int(input("DD: "))
+    year, month, day = 1999, 3, 26
     return datetime.date(year, month, day)
 
 def calculate_fill(birth):
@@ -45,37 +50,67 @@ def calculate_fill(birth):
     # subracting a boolean to get the behaviour of 0->0 else x->1
     return 52*years_alive - days_until_birthday//7 - (days_until_birthday%7 != 0)
 
+def generate_image(fill_count):
+    im = Image.new("RGB", CANVAS)
+    draw = ImageDraw.Draw(im)
 
-fill_count = calculate_fill(get_DOB())
+    draw.rectangle([0, 0, im.width, BORDER_THICKNESS-1], fill=WHITE)
+    draw.rectangle([0, im.height-1, im.width-1, im.height-BORDER_THICKNESS], fill=WHITE)
+    draw.rectangle([0, 0, BORDER_THICKNESS-1, im.height], fill=WHITE)
+    draw.rectangle([im.width-1, 0, im.width-BORDER_THICKNESS, im.height-BORDER_THICKNESS], fill=WHITE)
 
-im = Image.new("RGB", CANVAS)
-draw = ImageDraw.Draw(im)
+    px = im.load()
+    for y in range(GRID_HEIGHT):
+        for x in range(GRID_WIDTH):
+            if 52*y + x < fill_count:
+                px[BORDER_THICKNESS + BORDER_HSPACE + (DOT_WIDTH + DOT_HSPACE)*x, BORDER_THICKNESS + BORDER_VSPACE + (DOT_HEIGHT + DOT_VSPACE)*y] = GREY
+            else:
+                px[BORDER_THICKNESS + BORDER_HSPACE + (DOT_WIDTH + DOT_HSPACE)*x, BORDER_THICKNESS + BORDER_VSPACE + (DOT_HEIGHT + DOT_VSPACE)*y] = WHITE
 
-draw.rectangle([0, 0, im.width, BORDER_THICKNESS-1], fill=WHITE)
-draw.rectangle([0, im.height-1, im.width-1, im.height-BORDER_THICKNESS], fill=WHITE)
-draw.rectangle([0, 0, BORDER_THICKNESS-1, im.height], fill=WHITE)
-draw.rectangle([im.width-1, 0, im.width-BORDER_THICKNESS, im.height-BORDER_THICKNESS], fill=WHITE)
+    temp = Image.new("RGB", (im.width, im.height + TERMINAL_HEIGHT))
+    temp.paste(im, (0, TERMINAL_HEIGHT))
+    im = temp
 
-px = im.load()
-for y in range(GRID_HEIGHT):
-    for x in range(GRID_WIDTH):
-        if 52*y + x < fill_count:
-            px[BORDER_THICKNESS + BORDER_HSPACE + (DOT_WIDTH + DOT_HSPACE)*x, BORDER_THICKNESS + BORDER_VSPACE + (DOT_HEIGHT + DOT_VSPACE)*y] = GREY
-        else:
-            px[BORDER_THICKNESS + BORDER_HSPACE + (DOT_WIDTH + DOT_HSPACE)*x, BORDER_THICKNESS + BORDER_VSPACE + (DOT_HEIGHT + DOT_VSPACE)*y] = WHITE
+    draw = ImageDraw.Draw(im)
+    draw.rectangle([round((im.width - TERMINAL_WIDTH)/2), 0, im.width - TERMINAL_WIDTH, TERMINAL_HEIGHT], fill=WHITE)
 
-temp = Image.new("RGB", (im.width, im.height + TERMINAL_HEIGHT))
-temp.paste(im, (0, TERMINAL_HEIGHT))
-im = temp
+    temp = Image.new("RGB", (im.width + 2*PAD_WIDTH, im.height + 2*PAD_HEIGHT))
+    temp.paste(im, (PAD_WIDTH, PAD_HEIGHT))
+    im = temp
 
-draw = ImageDraw.Draw(im)
-draw.rectangle([round((im.width - TERMINAL_WIDTH)/2), 0, im.width - TERMINAL_WIDTH, TERMINAL_HEIGHT], fill=WHITE)
+    return im
 
-temp = Image.new("RGB", (im.width + 2*PAD_WIDTH, im.height + 2*PAD_HEIGHT))
-temp.paste(im, (PAD_WIDTH, PAD_HEIGHT))
-im = temp
+def four_digits(num):
+    return (4-len(str(num)))*"0" + str(num)
 
-im = im.resize((i*SCALE for i in im.size))
+def generate_video(filename):
+    im = generate_image(0)
+
+    size = (im.width*SCALE_VID, im.height*SCALE_VID)
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+
+    video = cv2.VideoWriter(filename, fourcc, 52, size)
+    video.write(cv2.cvtColor(numpy.array(im.resize(size)), cv2.COLOR_RGB2BGR))
+
+    px = im.load()
+    for y in range(GRID_HEIGHT):
+        for x in range(GRID_WIDTH):
+            px[BORDER_THICKNESS + BORDER_HSPACE + (DOT_WIDTH + DOT_HSPACE)*x + PAD_WIDTH, BORDER_THICKNESS + BORDER_VSPACE + (DOT_HEIGHT + DOT_VSPACE)*y + PAD_HEIGHT + TERMINAL_HEIGHT] = GREY
+            video.write(cv2.cvtColor(numpy.array(im.resize(size)), cv2.COLOR_RGB2BGR))
+
+    video.release()
+
+def compress_video(filename, quality):
+    copy = "temp.mp4"
+    copyfile(filename, copy)
+    subprocess.call("ffmpeg -i %s -vcodec libx264 -crf %i %s -y" % (copy, quality, filename))
+    os.remove(copy)
+
+im = generate_image(calculate_fill(get_DOB()))
+im = im.resize((i*SCALE_IMG for i in im.size))
 
 # im.show()
 im.save("life-charge.png", "PNG")
+
+generate_video("life-charge.mp4")
+compress_video("life-charge.mp4", 16)
